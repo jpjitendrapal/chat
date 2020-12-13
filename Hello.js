38980a1e5974f035cd69861e56f1459a0fefc8c0
jitendra <jpjitendrapal@gmail.com>
2020-12-13 16:29:24 +0530

    updated

--- Hello.js
+++ Hello.js
@@ -1,8 +1,9 @@
-﻿var http = require('http');
+﻿var port = process.env.PORT;
+var http = require('http');
 var myServer = http.createServer(function (request, response) {
     response.writeHeader(200, {"Content-Type":"text/html"});
     response.write("<b>Hello</b> World...:!");
     response.end();
 });
-myServer.listen(5000);
-console.log("Use port http://localhost:5000");
+myServer.listen(port);
+console.log("Use port http://localhost:"+port);
